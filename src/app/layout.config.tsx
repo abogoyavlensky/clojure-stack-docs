92af780db9fe6bfec2c2ab140b54384f14d5faef
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-18 02:27:20 +0000

    Change theme and add link to blog to the navbar

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -17,5 +17,9 @@ export const baseOptions: BaseLayoutProps = {
       url: '/docs',
       active: 'nested-url',
     },
+    {
+      text: 'Blog',
+      url: 'https://bogoyavlensky.com',
+    },
   ],
 };
