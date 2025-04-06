60e0c05f92adcea586c397b34fab5d0aafcab1e0
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-04-06 21:13:29 +0100

    Change url for og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/_next/static/media/og-image.png',
+        url: 'https://stack.bogoyavlensky.com/_next/static/media/og-image.png',
         width: 1200,
         height: 630,
         alt: 'Your documentation title',
